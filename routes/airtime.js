3c04e133e967e2578c3cc5d66fc499070a2f7254
KhalidMujahid <66231619+KhalidMujahid@users.noreply.github.com>
2024-12-25 08:06:23 -0800

    Update airtime.js

--- routes/airtime.js
+++ routes/airtime.js
@@ -25,15 +25,15 @@ airtimeRouter.post("/airtime/buy", authenticateToken, async (req, res) => {
             return res.status(400).json({ error: "User not found" });
         }
 
-        // Check user's balance
-        if (user.balance < amount) {
-            return res.status(400).json({ error: "Insufficient funds. Please fund your wallet." });
-        }
-
-        // Verify user's PIN
+         // Verify user's PIN
         const isPinValid = await bcrypt.compare(pin, user.pin);
         if (!isPinValid) {
             return res.status(400).json({ error: "Invalid PIN" });
+        }
+
+        // Check user's balance
+        if (user.balance < amount) {
+            return res.status(400).json({ error: "Insufficient funds. Please fund your wallet." });
         }
 
         // Prepare payload for the external API
